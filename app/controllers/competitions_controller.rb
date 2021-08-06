5df3e56e33305d7859985cac7467d47145191363
Caroline Tan <carolinectan3@gmail.com>
2021-08-05 21:24:30 -0400

    Refactor

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -6,21 +6,7 @@ class CompetitionsController < ApplicationController
   def show
     @competition = Competition.find(params[:id])
   end
-  # def new
-  #
-  # end
-  # def create
-  #
-  # end
-  # def edit
-  #
-  # end
-  # def update
-  #
-  # end
-  # def destroy
-  #
-  # end
+
   # private
   # def _params
   #   params.permit(:)
